3d0e99260fd884e39da26e7bb26ed2a62e5f1056
johan <stjernholm@msn.com>
2020-08-31 23:57:41 +0800

    Refactor: config environment variables

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const apiEndpoint = 'https://ov17500agf.execute-api.us-east-1.amazonaws.com/dev';
+const apiId = process.env.REACT_APP_API_ID
+
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
